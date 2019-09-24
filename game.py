b7c78f58b9e8c0cbfb7782734ab3e7de8edb849d
Jonny Smith <jonnyts235@gmail.com>
2019-09-24 10:18:46 -0600

    Added the user wallet and greeting

--- game.py
+++ game.py
@@ -8,7 +8,21 @@ weights = {
 }
 
 def casino_game():
-    
+    wallet = 0
+
+  def greeting():
+    print("Welcome to Jonny's Roulette")
+  def user_selection():
+    while True:
+      user_input = input("\nHow much money would you like to add to your wallet?")
+      wallet += input
+    if input != Number.isInteger(input):
+      print("Im sorry, must be an integer...")
+      continue
+    else:
+      print(f"You've deposited {wallet} into your wallet")
+      return wallet
+
 
     def roulette_function(weights):
         container_list = []
