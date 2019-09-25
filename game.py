85cdb4ec87196c219538154d9bd9af13c72fb984
Jonny Smith <jonnyts235@gmail.com>
2019-09-25 14:35:20 -0600

    Added win or loss function and connected all of the function with new args

--- game.py
+++ game.py
@@ -1,19 +1,19 @@
 import numpy as np
+def casino_game():
 
-
-weights = {
+  weights = {
   'red': 18,
   'green': 1,
   'black': 18
-}
-
-def casino_game():
+  }
   wallet = 0
+
 
   def greeting():
     print("Welcome to Jonny's Roulette")
 
   def user_selection():
+    global wallet
     print("Here are our menu options")
     user_input = input("Please select from the following wallet choices: \n1: $50\n2: $100\n3: $150\n4: $200\n5: $250\n\nWhat would you like? ")
     if user_input == "1":
@@ -38,7 +38,8 @@ def casino_game():
 
       
   def amount_wagered():
-    wager_amount = input("How much would you like to wager?")
+    global wallet
+    print("How much would you like to wager?")
     user_input = input("Please select from the following wager choices: \n50: $50\n100: $100\n150: $150\n200: $200\n250: $250\n\nWhat would you like? ")
     if user_input == "0":
       wallet -= 50
@@ -67,24 +68,33 @@ def casino_game():
     
 
   def choice_of_color(weights):
-      user_choice = input("\nWhat color would you like put bet on?")
-      choice_selection = weights.get(user_choice, False)
-      if choice_selection == False:
-        print("Im sorry that wasn't one of the colors...")
-        continue
-      else:
-        print(f"You picked {choice_selection} no takebacks")
-        return choice_selection[1]
-      roulette_function(weights)
+    user_choice = input("\nWhat color would you like put bet on?")
+    choice_selection = weights.get(user_choice, False)
+    if choice_selection == False:
+      print("Im sorry that wasn't one of the colors...")
+      choice_of_color(weights)
+    else:
+      print(f"You picked {choice_selection} no takebacks")
+      return choice_selection[1]
+    roulette_function(weights, user_choice, user_choice)
 
-  def roulette_function(weights):
-      container_list = []
+  def roulette_function(weights, user_choice, user_input):
+    container_list = []
 
-      for (name, weight) in weights.items():
-          for _ in range(weights):
-              container_list.append(name)
+    for (name, weight) in weights.items():
+      for _ in range(weights):
+        container_list.append(name)
 
-      return np.random.choice(container_list)
+    outcome = np.random.choice(container_list)
+    win_or_loss(outcome, user_choice, user_input)
+
+  def win_or_loss(outcome, user_choice, user_input):
+    global wallet
+    if user_choice == outcome:
+      wallet += user_input * 2
+      print(f"CONGRATS!!! You picked {user_choice}, which was the winning color. ")
+    else:
+      print("Sorry your choice wasn't the right one... You're broke")
 
 
-  
+casino_game()
