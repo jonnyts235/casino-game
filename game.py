e06bfb50b41359dfcb3a94803e651a3151d266e7
Jonny Smith <jonnyts235@gmail.com>
2019-09-25 08:57:08 -0600

    Redid the user selection function

--- game.py
+++ game.py
@@ -14,34 +14,47 @@ def casino_game():
     print("Welcome to Jonny's Roulette")
 
   def user_selection():
-    while True:
-      user_input = input("\nHow much money would you like to add to your wallet?")
-      wallet += input
-    if input != Number.isInteger(input):
-      print("Im sorry, must be an integer...")
+    print("Here are our menu options")
+    user_input = input("Please select from the following menu choices: \n1: Deposit\n2: Balance\n3: Withdraw\n4: Make a bill payment\n5: Quit \n\nWhat would you like? ")
+    if user_input == "50":
+      wallet += 50
+      print(f"You currently have ${wallet}")
+    elif user_input == "100":
+      wallet += 100
+      print(f"You currently have ${wallet}")
+    elif user_input == "150":
+      wallet += 150
+      print(f"You currently have ${wallet}")
+    elif user_input == "200":
+      wallet += 200
+      print(f"You currently have ${wallet}")
+    elif user_input == "250":
+      wallet += 250
+      print(f"You currently have ${wallet}")
+    else: 
+      print("That isnt a valid option, please try again")
+      user_selection()
+
+    
+
+  def choice_of_color(weights):
+    user_choice = input("\nWhat color would you like put bet on?")
+    choice_selection = weights.get(user_choice, False)
+    if choice_selection == False:
+      print("Im sorry that wasn't one of the colours...")
       continue
     else:
-      print(f"You've deposited {wallet} into your wallet")
-      return wallet
-
-    def choice_of_color(weights):
-      user_choice = input("\nWhat color would you like put bet on?")
-      choice_selection = weights.get(user_choice, False)
-      if choice_selection == False:
-        print("Im sorry that wasn't one of the colours...")
-        continue
-      else:
-        print(f"You picked {choice_selection} no takebacks")
-        return choice_selection[1]
+      print(f"You picked {choice_selection} no takebacks")
+      return choice_selection[1]
       
-      def amount_wagered()
+  def amount_wagered()
 
+  def roulette_function(weights):
+      container_list = []
 
-    def roulette_function(weights):
-        container_list = []
+      for (name, weight) in weights.items():
+          for _ in range(weights):
+              container_list.append(name)
 
-        for (name, weight) in weights.items():
-            for _ in range(weights):
-                container_list.append(name)
-
-        return np.random.choice(container_list)
+      return np.random.choice(container_list)
+  
